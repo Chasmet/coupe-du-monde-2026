8446965f653e19b23829515d1aa827c64e524560
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-07-07 10:20:43 +0200

    Forcer cache arbre bleu officiel

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='cdm2026-cache-v25-arbre-bleu-officiel';
+const CACHE_NAME='cdm2026-cache-v26-arbre-bleu-officiel-mobile';
 self.addEventListener('install',event=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method==='GET')event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)))});
